b39bc79e29c7a3a718fdd752a56e2227aa05c661
charlesmariga <charlesmariga37@gmail.com>
2022-07-24 21:59:12 +0300

    Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
  * bubble_sort - bubble sort
@@ -11,4 +12,26 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	int swapped = 1;
+	int i = 0;
+	int temp;
+
+	if (!array)
+		return;
+
+	while (swapped == 1)
+	{
+		swapped = 0;
+		for (i = 1; i < (int)size; i++)
+		{
+			if (array[i - 1] > array[i])
+			{
+				temp = array[i - 1];
+				array[i - 1] = array[i];
+				array[i] = temp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+	}
 }
