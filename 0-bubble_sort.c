21aeb15a850a5b9f0a896ad49f6ae4c3945fb030
charlesmariga <charlesmariga37@gmail.com>
2022-07-24 08:05:42 +0300

    Initial commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort
+ * @array: array of integers
+ * @size: size of the array
+ *
+ * Description: bubble sort algorithm for an integer array
+ *
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+}
